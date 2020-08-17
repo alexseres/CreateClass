ce3694ecfe005c94a3dda3304cea3e3040a872bf
alexseres <lilaalex95@gmail.com>
2020-08-17 01:50:50 -0700

    this commit is about to check if git is working properly

---
+++ CreateClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateClass
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Trial");
+        }
+    }
+}
